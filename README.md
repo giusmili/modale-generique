bc76a61187f7a369188d12c91bd1d5df4c3a8314
giusmili <giusmili67@gmail.com>
2023-12-23 11:12:05 +0100

    first commit modal

---
+++ README.md
@@ -0,0 +1,2 @@
+# 🥉 Modal-Window générique
+> On crée une fenêtre modale avec des évenement de base javascript
---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+import { User } from "./modale.js"
+
+window.onload = () => {
+    // On récupère tous les boutons d'ouverture de modale
+   
+
+    /* appel de la class */
+
+        User.modale()
+}
